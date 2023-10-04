e6193e56ec92707844a52226f0bfb4f7ad13fa56
KIRA MASTER <47713860+Kira-Master@users.noreply.github.com>
2023-10-04 22:19:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,7 +21,7 @@ require('dotenv').config()
 
 creator = '乂ZEXXA乂' // Nama 
 port = 8080 //port host
-LimitApikey = 100 // Limit Apikey default
+LimitApikey = 1000 // Limit Apikey default
 
 //___________ ┏  Database Mongodb Setting  ┓ ___________\\
 
@@ -30,7 +30,7 @@ keymongodb = process.env.mongodb //example "mongodb+srv://user:password@name_dat
 //___________ ┏  SMTP Setting  ┓ ___________\\
 
 //note service SMTP Gmail Tidak Boleh Send Kat Temp Email Kalau mahu pakai temp email buat di service lain
-usetempemail = false // kalau true boleh sing up pakai email temp kalau pakai service gmail pakai false je 
+usetempemail = true // kalau true boleh sing up pakai email temp kalau pakai service gmail pakai false je 
 servicesmtp = 'Gmail'//Ada gmail ada sendinblue ikut masing2 limit gmail 500 limit sendinblue 300 free. List service smtp : https://nodemailer.com/smtp/well-known/
 sendemail = process.env.sendemail //Email SMTP
 fromsendemail = process.env.fromsendemail // From Email SMTP *kalau takde custom email kena sama email dengan Email SMTP
@@ -94,7 +94,7 @@ loghandler = {
     instgram: {
 	    status: false,
 	    code: 403,
-	    message: '[!] Forbiden or Error, Username tidak wujud atau account private',
+	    message: '[!] Forbiden or Error, Username tidak ditemukan atau account private',
 	    maintanied_by: `${creator}`
    },
     register: {
