02f0b2ded647c9e5a96706eb0f9db7770185f520
KIRA MASTER <47713860+Kira-Master@users.noreply.github.com>
2023-10-09 04:16:17 +0700

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -25,7 +25,7 @@
 
 //Visit
 
-$.getJSON("https://api.countapi.xyz/hit/alpis.eu.org/visitor", function(response) {
+$.getJSON("https://api.countapi.xyz/hit/apu.zexxa.tech/visitor", function(response) {
     $("#visitor").text(response.value);
 })
 
